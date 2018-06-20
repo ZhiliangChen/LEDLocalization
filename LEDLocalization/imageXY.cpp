d6b287c9cd033d88cf39f0ecef3409781c0d0680
ZhiliangChen <634670289@qq.com>
2018-06-20 11:18:12 +0800

    6-20

--- LEDLocalization/imageXY.cpp
+++ LEDLocalization/imageXY.cpp
@@ -1,1 +1,21 @@
 #include "stdafx.h"
+#include <cv.h>
+#include <highgui.h>
+#include "imageXY.h"
+
+void CvImageXY::ShowImage()
+{
+
+	img = cvLoadImage("RawToRGBData.bmp", 1);
+	cvNamedWindow("circles", 1);
+	cvShowImage("circles", img);
+
+}
+
+void CvImageXY::FindPoint()
+{
+
+
+
+
+}
--- LEDLocalization/imageXY.h
+++ LEDLocalization/imageXY.h
@@ -1,1 +1,17 @@
 #pragma once
+
+class CvImageXY
+{
+
+public:
+	void ShowImage();
+	void FindPoint();
+	IplImage* img;
+	IplImage* gray;
+	CvMemStorage* storage;
+	CvSeq* circles;
+	int x_center;
+	int y_center;
+
+
+};
